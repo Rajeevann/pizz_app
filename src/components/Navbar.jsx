80b8a77e4ad9e693b058da9f745d7077bce15565
Rajeevan <rajee.srs@gmail.com>
2023-07-16 22:54:56 -0700

    Assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'>
+            <img src={}/>
+        </div>
+        <div className="rightSide"></div>
+    </div>
+  )
+}
+
+export default Navbar
