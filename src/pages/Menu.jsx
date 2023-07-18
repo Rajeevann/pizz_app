a3d4cef2eb32190d831fef232eac7f350ce01783
Rajeevan <rajee.srs@gmail.com>
2023-07-18 09:45:41 -0700

    Menu Styling

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../components/MenuItem";
+import "../styles/Menu.css";
 
 const Menu = () => {
   return (
