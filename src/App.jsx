b0317bde145f55cd1b00f5d9fe574c6b46ff53a5
Rajeevan <rajee.srs@gmail.com>
2023-07-18 09:57:34 -0700

    Contact  Us Page & Styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Menu from "../src/pages/Menu";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
